8863a112b6ece18c2062f6e7cb4d44bf23114048
HannanAmar05 <amarhannan358@gmail.com>
2026-04-07 12:23:43 +0700

    updating env

--- src/libs/firebase/firebase.ts
+++ src/libs/firebase/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZRdsdU-6bMG-DHnrI8bRGuWtQSBteeD8",
-  authDomain: "social-auth-implementation.firebaseapp.com",
-  projectId: "social-auth-implementation",
-  storageBucket: "social-auth-implementation.firebasestorage.app",
-  messagingSenderId: "657554681819",
-  appId: "1:657554681819:web:488d95c3f92880ad0441fe",
-  measurementId: "G-DH56NFTH01",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
